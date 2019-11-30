b05f50cba2d883f47b97f09f2c6c3e48130d8965
huxy <huxaiphaeridris@gmail.com>
2019-11-30 17:09:37 -0500

    add unit tests

--- tests/test.py
+++ tests/test.py
@@ -1,2 +1,43 @@
-from scrapy import Spider, Request
-from scrapy.http import HtmlResponse
+import subprocess
+import sys
+import unittest
+
+from scrapy import Spider
+from scrapy.http import Request
+
+from jaleelscrapper.jaleelscrapper.items import JaleelscrapperItem
+from jaleelscrapper.jaleelscrapper.spiders.flightSpider import FlightDestinationSpider
+
+
+class TestParsers(unittest.TestCase):
+
+    def setUp(self):
+        self.item = JaleelscrapperItem()
+
+    def test_destination(self):
+        """Test destination filed """
+        expected = "New York"
+        result = self.item['destination'] = "New York"
+        self.assertEqual(result, expected)
+
+    def test_time(self):
+        """Test time filed """
+        expected = "12:00:00"
+        result = self.item['time'] = "12:00:00"
+        self.assertEqual(result, expected)
+
+    def test_temperature(self):
+        """Test temperature filed """
+        expected = 23
+        result = self.item['temperature'] = 23
+        self.assertEqual(result, expected)
+
+    def test_note(self):
+        """Test note filed """
+        expected = "!Que frio!"
+        result = self.item['note'] = "!Que frio!"
+        self.assertEqual(result, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
